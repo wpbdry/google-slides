d1f0b4e5a20b8c91a1f07fd689cfeff26e0bec8e
William Dry <wpbdry@gmail.com>
2020-03-23 12:23:25 +0100

    improve HTTPError

--- errors/api-errors.js
+++ errors/api-errors.js
@@ -1,15 +1,22 @@
 import BaseError from './base-errors.js'
+import Gaxios from 'gaxios'
 
 export class HTTPError extends BaseError {
     /**
      * 
-     * @param {{}} response The object caught from the service method.
+     * @param {GaxiosError} error The object caught from the service method.
      */
-    constructor(response) {
-        const { code, errors } = response
-        super(`API error (${code}): ${errors[0].message}`)
-        this.code = code
-        this.errorMessage = errors[0].message
-        this.errors = errors
+    constructor(error) {
+        // if expected format
+        if (error instanceof Gaxios.GaxiosError) {
+            const { code, errors } = error
+            super(`API error (${code}): ${errors[0].message}`)
+            this.code = code
+            this.errorMessage = errors[0].message
+            this.errors = errors
+            return
+        }
+        // if unexpected format
+        this.error = error
     }
 }
