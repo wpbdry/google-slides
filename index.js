cee67c2c6a2817be2eb5c4aa5f13ad6930dcdbf7
William Dry <wpbdry@gmail.com>
2020-03-23 13:28:21 +0100

    remove commented notes

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 export { API } from './api.js'
 export { Presentation } from './presentation.js'
-
-// NOTES:
-
-// TODO: set up proper logging / error handling
-// TODO: publish on NPM
-// TODO: CI/CD?
